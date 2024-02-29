32c8d8b662492d1872c61ddc06c1a573351de0db
unknown <alina.cernacovschi@gmail.com>
2024-02-28 18:47:58 -0600

    variables and datatypes

---
+++ Intro_to_Js/01_variable_dataType/variable.js
@@ -0,0 +1,16 @@
+// 1. variable name that assigns a string value of my first name
+let name = 'Alina';
+//2. variable age that assigns a number value of my age
+let age = 26
+//3. variable isStudent that assigns a boolean value
+let isStudent = false
+//4. variable favoriteColors that assigns an array of string values
+let favoriteColors = ['Black', 'Red', 'Pink', 'Blue']
+// 5. variable person that assigns an object
+let person = {
+    name : 'Alina',
+    age : '26',
+    isStudent: false
+}
+
+console.log(person);
