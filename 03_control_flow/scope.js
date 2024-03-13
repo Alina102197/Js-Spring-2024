ab7709af0c819987fb3042e666c957e7b16ed731
unknown <alina.cernacovschi@gmail.com>
2024-03-12 23:51:07 -0500

    Variable scope

---
+++ 03_control_flow/scope.js
@@ -0,0 +1,6 @@
+//Validate user input:
+// Global scope:
+let userInput = null;
+if(userInput === undefined || userInput === null) {
+    console.log('Please enter a valid user input');
+} else { console.log('Thank you for submitting the form'); }
