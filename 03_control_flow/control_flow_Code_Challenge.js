10a04064fec0458e0fa264f2832f7dcf72952d5f
unknown <alina.cernacovschi@gmail.com>
2024-03-12 23:30:52 -0500

    Control Flow Code Challenge

---
+++ 03_control_flow/control_flow_Code_Challenge.js
@@ -0,0 +1,37 @@
+// 1.Write a program that is based on a number and displays whether the number is odd or even using an if-else statement.
+// let number = 10;
+// if(number % 2 ===0) {
+//     console.log(`number ${number} is even`);
+// } else { console.log(`number ${number} is odd`);}
+
+// 2. Write a program based on age and displays a message based on their age using an if-else if statement:
+// let age = 24;
+// if(age < 18) {
+//     console.log('You are a minor');
+// } else if(age >=18 && age <=65) {
+//     console.log('You are an adult');
+// } else { console.log ('You are a senior citizen')}
+
+// 3. Write a program based on a letter and displays whether the letter is a vowel or a consonant using an if-else statement:
+// let letter = 'e';
+// if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+//     console.log(`Letter ${letter} is a vowel`);
+// } else { console.log(`Letter ${letter} is a consonant`); }
+
+// 4. Write a program based on three numbers and displays the largest number using an if-else statement.
+// let numberOne = 30;
+// let numberTwo = 15;
+// let numberThree = 45;
+// if(numberOne > numberTwo && numberOne > numberThree) {
+//     console.log(`The largest number is ${numberOne}`);
+// } else if(numberTwo > numberOne && numberTwo > numberThree) {
+//     console.log(`The largest number is ${numberTwo}`);
+// } else { console.log(`The largest number is ${numberThree}`); }
+
+// 5. Write a program based on given password and displays a message based on the password using an if-else statement:
+let password = 'Test1233';
+let passwordLength = password.length;
+if(passwordLength < 8) {
+    console.log('Password too short');
+} else { console.log('Password rejected'); }
+
---
+++ 03_control_flow/ifElseStatement.js
@@ -0,0 +1,37 @@
+// if(condition) {
+//     //If condition is true, all of the code within this block will get executed
+// }\
+
+//Build a program that tells the user if they are eligible for vote based on age variable
+// let age  = 17;
+// if(age >= 18) {
+//     console.log('You are eligible to vote: ' + age)
+// } else {
+//     console.log('You are not eligible to vote: ' + age)
+// }\
+
+//Check whether a number is even or odd:
+// let number  = 10;
+// if (number % 2 === 0) {
+//     console.log(`${number} is even`);
+// } else { console.log(`${number} is odd `); }
+
+//Validate user input:
+// let userInput = null;
+// if(userInput === undefined || userInput === null) {
+//     console.log('Please enter a valid user input');
+// } else { console.log('Thank you for submitting the form'); }
+
+//Grade calculator:
+let grade = 50;
+if(grade >= 90) {
+    console.log('Your grade is A');
+} else if(grade >= 80) {
+    console.log('Your grade is B');
+} else if(grade >= 70) {
+    console.log('Ypur grade is C');
+} else if(grade >= 60) {
+    console.log ('Your grade is D');
+} else {
+    console.log('You failed!');
+}
